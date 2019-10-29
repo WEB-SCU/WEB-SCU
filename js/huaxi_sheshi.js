5570f9209ff87fc4038c771283dc333a51745464
DOCCAO <2947323341@qq.com>
2019-10-29 16:08:09 +0800

    上传到GitHub

---
+++ js/huaxi_sheshi.js
@@ -0,0 +1,16 @@
+var touristSpots = [{
+    name: '钟楼',
+    position: [104.061421,30.63815],
+    zIndex: 300,
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [128, 160],
+    ancher: [64, 153],
+}, {
+    name: '东大门',
+    position: [104.066032,30.641088],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [146, 144],
+    ancher: [73, 140],
+}	   ];
