f5613266cc82e6cccf79366214f432b696fc753b
DOCCA0 <52144831+DOCCA0@users.noreply.github.com>
2019-11-02 16:33:19 +0800

    第三次更新了

---
+++ js/jiangan_sheshi.js
@@ -0,0 +1,29 @@
+var touristSpots = [{
+    name: '东门',
+    position: [104.007401,30.560676],
+    zIndex: 300,
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [128, 160],
+    ancher: [64, 153],
+}, {
+    name: '不高山',
+    position: [103.994071,30.559464],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [146, 144],
+    ancher: [73, 140],
+}, 
+			
+					
+	{
+    name: '第三食堂',
+    position: [104.003654,30.561487],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [146, 144],
+    ancher: [73, 140],
+}
+				   
+	
+				   ];
---
+++ js/wangjiang_sheshi.js
@@ -0,0 +1,25 @@
+var touristSpots = [{
+    name: '革命烈士纪念碑',
+    position: [104.089953,30.629975],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [128, 160],
+    ancher: [64, 153],
+}, {
+    name: '体育馆',
+    position: [104.083841,30.628625],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [146, 144],
+    ancher: [73, 140],
+}, 
+		
+					
+	{
+    name: '北门',
+    position: [104.077458,30.634195],
+    smallIcon: '../../img/small.png',
+	bigIcon: '../../img/big.png',
+    size: [146, 144],
+    ancher: [73, 140],
+	} ];
